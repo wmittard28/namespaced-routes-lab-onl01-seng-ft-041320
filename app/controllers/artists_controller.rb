4a4a9e3288c39e5833ac80ddd9a2a1f43247c051
wmittard28 <billymittard@ymail.com>
2020-06-19 12:16:58 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,30 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @preference = Preference.first
+
+    if @preference
+      if @preference.artist_sort_order = "ASC"
+        @artists = Artist.all.sort_by{|artist| artist.name}
+      else
+        @artists = Artist.all.sort_by{|artist| artist.name}.reverse
+      end
+    else
+      @artists = Artist.all
+    end
   end
+
 
   def show
     @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    @preference = Preference.first
+    if @preference.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Preference, type: :model do
-  it "has the right fields" do
-    p = Preference.new(allow_create_artists: false, allow_create_songs: true)
-    expect(p.allow_create_artists).to eq false
-    expect(p.allow_create_songs).to eq true
-  end
+  
 end
